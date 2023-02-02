62dad7d89566c7ae8f6df6f94d05ed7393a33330
emilytm <113457923+emilytm@users.noreply.github.com>
2023-02-02 17:30:37 -0500

    Adding component files and started Info

---
+++ src/components/Info.jsx
@@ -0,0 +1,14 @@
+export default function Info(){
+    return (
+
+        <div className="info">
+            <h1 className="info--name">Emily Taintor McIntyre</h1>
+            <h2 className="info--title">Frontend Developer</h2>
+            <div className="info--buttons">
+                <button className="info--buttons--email">Email</button>
+                <button className="info--buttons--linkedin">LinkedIn</button>
+            </div>
+        </div>
+
+    )
+}
